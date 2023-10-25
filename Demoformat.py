b12a15cb42f432e613b487edeb50cde4fff8fb4a
ParkJooYoungman <dmmzmst@naver.com>
2023-10-25 10:06:58 +0900

    파일연습 완료

--- Demoformat.py
+++ Demoformat.py
@@ -1,10 +1,30 @@
+
+
+
+
 #파일쓰기
-f = open("domo.txt", "wt", encoding="utf-8")
-f.write("첫번쨰\e두번쨰\n세번쨰\n")
+f = open("demo.txt", "wt", encoding="utf-8")
+f.write("첫번쨰\n두번쨰\n세번쨰\n")
+f.close()
+#기존파일에 첨부
+f = open("demo.txt", "wt", encoding = "utf-8")
+f.write("다른 내용을 첨부\n")
 f.close()
 
 #파일읽기
 f = open("demo.txt", "rt", encoding="utf-8")
 result = f.read()
 print(result)
+
+
+#처음으로 리셋
+f.seek(0)
+print(f.readline(),end="")
+print(f.readline(),end="")
+
+f.seek(0)
+lst = f.readlines()
+for item in lst:
+    print(item, end="")
+
 f.close()
